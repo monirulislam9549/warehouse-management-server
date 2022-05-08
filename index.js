e84ba479cc7b1c767383fa3f04b4fa114f5d122b
monirulislam9549 <monirulislam9549@gmail.com>
2022-05-08 11:52:33 +0600

    item load

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,13 +12,34 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ts9so.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('mobile connected');
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+    try {
+        await client.connect();
+        const mobileCollection = client.db('mobileWarehouse').collection('mobile');
+
+        app.get('/mobile', async (req, res) => {
+            const query = {};
+            const cursor = mobileCollection.find(query);
+            const mobiles = await cursor.toArray();
+            res.send(mobiles);
+        })
+
+        app.get('/mobile/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const mobile = await mobileCollection.findOne(query);
+            res.send(mobile);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('anything you can write')
